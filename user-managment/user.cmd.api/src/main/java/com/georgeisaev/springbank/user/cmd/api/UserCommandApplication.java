246f66c5253d793b09ae09ca89d4c97d44eca7fe
Исаев Георгий Геннадьевич <isaev.georgii@otr.ru>
2021-09-26 23:19:02 +0300

    axon configuration

--- user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/UserCommandApplication.java
+++ user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package com.georgeisaev.springbank.user.cmd.api;
 
+import com.georgeisaev.springbank.usercore.event.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
     public static void main(String[] args) {
---
+++ user-managment/user.core/src/main/java/com/georgeisaev/springbank/usercore/event/AxonConfig.java
@@ -0,0 +1,79 @@
+package com.georgeisaev.springbank.usercore.event;
+
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoSettingsFactory;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.spring.config.AxonConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Configuration
+public class AxonConfig {
+
+    @Value("spring.data.mongodb.host:localhost")
+    String mongoHost;
+
+    @Value("spring.data.mongodb.port:27017")
+    int mongoPort;
+
+    @Value("spring.data.mongodb.database:user")
+    String mongoDatabase;
+
+    @Bean
+    public MongoClient mongoClient() {
+        var mongoFactory = new MongoFactory();
+        var mongoSettingsFactory = new MongoSettingsFactory();
+        mongoSettingsFactory.setMongoAddresses(List.of(new ServerAddress(mongoHost, mongoPort)));
+        mongoFactory.setMongoClientSettings(mongoSettingsFactory.createMongoClientSettings());
+        return mongoFactory.createMongo();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return DefaultMongoTemplate.builder()
+                .mongoDatabase(mongoClient(), mongoDatabase)
+                .build();
+    }
+
+    @Bean
+    public TokenStore tokenStore(Serializer serializer) {
+        return MongoTokenStore.builder()
+                .mongoTemplate(mongoTemplate())
+                .serializer(serializer)
+                .build();
+    }
+
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient mongoClient) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(DefaultMongoTemplate.builder()
+                        .mongoDatabase(mongoClient)
+                        .build())
+                .build();
+    }
+
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
+                .build();
+    }
+
+}
