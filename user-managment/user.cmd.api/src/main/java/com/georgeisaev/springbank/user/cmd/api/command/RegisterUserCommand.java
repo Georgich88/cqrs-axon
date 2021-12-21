910050c2ead02d0ba166cf6db91acd59c7b75428
Исаев Георгий <isaev.georgii@otr.ru>
2021-12-21 09:01:38 +0300

    user commands

---
+++ user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/command/RegisterUserCommand.java
@@ -0,0 +1,22 @@
+package com.georgeisaev.springbank.user.cmd.api.command;
+
+import com.georgeisaev.springbank.usercore.model.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RegisterUserCommand {
+
+    @TargetAggregateIdentifier
+    String id;
+
+    User user;
+
+
+
+}
