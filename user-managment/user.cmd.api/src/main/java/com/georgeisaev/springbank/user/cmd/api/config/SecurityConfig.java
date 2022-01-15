5198043de065414e255959a5b0c10372c70b3c97
Georgy Isaev <isaev.georgii@otr.ru>
2022-01-15 10:27:40 +0300

    javadoc

--- user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/config/SecurityConfig.java
+++ user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/config/SecurityConfig.java
@@ -10,17 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
-     * Override this method to configure the {@link HttpSecurity}. Typically subclasses
-     * should not invoke this method by calling super as it may override their
-     * configuration. The default configuration is:
-     *
-     * <pre>
-     * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
-     * </pre>
-     * <p>
-     * Any endpoint that requires defense against common vulnerabilities can be specified
-     * here, including public ones. See {@link HttpSecurity#authorizeRequests} and the
-     * `permitAll()` authorization rule for more details on public endpoints.
+     * Configures the {@link HttpSecurity}.
      *
      * @param http the {@link HttpSecurity} to modify
      * @throws Exception if an error occurs
