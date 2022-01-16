18fca20f048348f8b3f98d9d3b1d9d1ae0d749b8
Georgy Isaev <isaev.georgii@otr.ru>
2022-01-16 14:49:18 +0300

    REST Controller for Removing Users

---
+++ user-managment/user.cmd.api/src/main/java/com/georgeisaev/springbank/user/cmd/api/controller/RemoveUserController.java
@@ -0,0 +1,43 @@
+package com.georgeisaev.springbank.user.cmd.api.controller;
+
+import com.georgeisaev.springbank.user.cmd.api.command.RemoveUserCommand;
+import com.georgeisaev.springbank.user.cmd.api.dto.BaseResponse;
+import com.georgeisaev.springbank.user.cmd.api.dto.RegisterUserResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.slf4j.helpers.MessageFormatter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RestController
+@RequestMapping(path = "/api/v1/remove-user")
+public class RemoveUserController {
+
+    private static final String MSG_ERR_REMOVE_REQUEST = "Error while processing remove user request for id {}";
+    private static final String MSG_INFO_USER_SUCCESSFULLY_REMOVED = "User successfully removed";
+
+    CommandGateway commandGateway;
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<BaseResponse> removeUser(@PathVariable String id) {
+        RemoveUserCommand command = new RemoveUserCommand(id);
+        try {
+            commandGateway.sendAndWait(command);
+            return ResponseEntity.ok(new BaseResponse(MSG_INFO_USER_SUCCESSFULLY_REMOVED));
+        } catch (Exception e) {
+            String safeErrorMessage = MessageFormatter.format(MSG_ERR_REMOVE_REQUEST, command.getId()).getMessage();
+            log.error(safeErrorMessage, e);
+            return ResponseEntity.internalServerError().body(new RegisterUserResponse(safeErrorMessage));
+        }
+    }
+
+}
