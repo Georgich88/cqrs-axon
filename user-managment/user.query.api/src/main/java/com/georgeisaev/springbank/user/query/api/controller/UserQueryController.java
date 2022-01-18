cff7e5f0a7ed17f0119e63e1974b291950cb742a
Georgy Isaev <isaev.georgii@otr.ru>
2022-01-18 13:52:51 +0300

    User Query Controller

--- user-managment/user.query.api/src/main/java/com/georgeisaev/springbank/user/query/api/controller/UserQueryController.java
+++ user-managment/user.query.api/src/main/java/com/georgeisaev/springbank/user/query/api/controller/UserQueryController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.net.URI;
 
 import static java.util.Objects.isNull;
 
