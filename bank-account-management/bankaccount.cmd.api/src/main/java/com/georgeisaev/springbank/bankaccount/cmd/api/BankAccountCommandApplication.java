b3fea71c94c422648e79970b9d571cd05d029a10
Georgy Isaev <isaev.georgii@otr.ru>
2022-02-12 11:01:18 +0300

    Bank Account Project

---
+++ bank-account-management/bankaccount.cmd.api/src/main/java/com/georgeisaev/springbank/bankaccount/cmd/api/BankAccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.springbank.bankaccount.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountCommandApplication.class, args);
+    }
+
+}
---
+++ bank-account-management/bankaccount.cmd.api/src/test/java/com/georgeisaev/springbank/bankaccount/cmd/api/BankAccountCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.springbank.bankaccount.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
